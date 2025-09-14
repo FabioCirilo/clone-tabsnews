b973b7081853ec7b9774acdd7a3d726e303b67fe
FabioCirilo <103139712+FabioCirilo@users.noreply.github.com>
2025-09-14 07:45:08 +0000

    Mudança no titulo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Fábio, seja um bom programador pois isso vsi mudar a tua vida</h1>
+    return <h1>Prof Moser, sou muito grato por ter te conhecido, pois foste um grande incentivo para que eu podesse me apaixonar pela programação, hoje trabalho com isso e estou muito feliz</h1>
 }
 
 export default Home;
