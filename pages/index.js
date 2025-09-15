c9f0b40166d5ea236d9ad267584cf153cb1c43c5
FabioCirilo <103139712+FabioCirilo@users.noreply.github.com>
2025-09-15 07:25:39 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Prof Moser, sou muito grato por ter te conhecido, pois foste um grande incentivo para que eu podesse me apaixonar pela programação, hoje trabalho com isso e estou muito feliz</h1>
+    return <h1>Benildo, vamos programar pois isso vai nos deixar ricos</h1>
 }
 
 export default Home;
